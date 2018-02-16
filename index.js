fc8803d526a7b0b1fdea0aee9c508dfd5fbc1cf7
bruno78 <devbruno78@gmail.com>
2018-02-16 18:43:22 -0500

    Finish cleaning the code, and revising comments

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = proces
 const BASE_API_URL = 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1';
 const ERROR_MESSAGE = "Film id not valid or key missing";
 
-// const db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, err => {
-//   if (err) throw err; 
-//   console.log("Successfully connected to Database");
-// });
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'sqlite',
@@ -45,22 +40,22 @@ function getFilmRecommendations(req, res) {
   /**
    * TODO: 1. Recommended films must have:
    * DONE: The same genre as the parent film
-   * IMPLEMENT: A mininum of 5 reviews
-   * IMPLEMENT: An average rating greater than 4.0
+   * DONE: A mininum of 5 reviews
+   * DONE: An average rating greater than 4.0
    * DONE: Been released within 15 years, before or after the
    * the parent film
    * IMPLEMENT: A sort order based on film id (order by film id)
    */
 
    /**
-    * TODO: 2. The application should allow developers to:
+    * COMPLETED: 2. The application should allow developers to:
     * DONE: Paginate by offset
     * DONE: Limit the number of returned records
     */
 
     /**
      * TODO: 3. The application should handler for:
-     * IMPLEMENT: Client/server failure
+     * DONE: Client/server failure
      * IMPLEMENT: Missing routes
      */
 
@@ -82,6 +77,7 @@ function getFilmRecommendations(req, res) {
     offset = parseInt(req.query.offset);
   }
 
+  // This query returns the id, title, release date and genre of ALL the movies that has the same genre and it's within 15 years before and after the parent film's release date.
   sequelize.query(`SELECT films.id, films.title, films.release_date AS releaseDate, genres.name AS genre FROM films 
     INNER JOIN genres ON films.genre_id = genres.id
     WHERE genre_id = (SELECT genre_id FROM films WHERE id = :id) 
@@ -113,7 +109,8 @@ function getFilmRecommendations(req, res) {
           // console.log(reviews);
 
           // Add reviews from 3rd party api to the results list
-          // Get the reives
+          // Get the number of reviews
+          // Get the average ratings from the reviews. 
           for(let k = 0; k < results.length; k++) {
             if(results[k].id === reviews[k].film_id) {
               results[k].reviews = reviews[k].reviews.length;
@@ -121,14 +118,12 @@ function getFilmRecommendations(req, res) {
             }
           }
 
-          // console.log(results);
           // filtering results by review length and average ratings
           let filteredResults = results.filter( (result) => {
             return result.reviews >= 5 && reviews.getAvgRating > 4.0;
           });
 
-          console.log(filteredResults);
-
+          // Build the recommended film list
           let recommendedFilmList = [];
           filteredResults.forEach( film => {
             recommendedFilmList.push({
@@ -152,91 +147,6 @@ function getFilmRecommendations(req, res) {
         res.status(422).json({message: ERROR_MESSAGE});
       });
 
-  // This query returns the id, title, release date and genre of ALL the movies that has the same genre and it's within 15 years before and after the parent film's release date.
-  // db.all(
-  //   `SELECT films.id, films.title, films.release_date AS releaseDate, genres.name AS genre FROM films 
-  //   INNER JOIN genres ON films.genre_id = genres.id
-  //   WHERE genre_id = (SELECT genre_id FROM films WHERE id = :id) 
-  //   AND
-  //     films.release_date >= date((SELECT films.release_date FROM films WHERE id = :id),'-15 years') 
-  //   AND
-  //     films.release_date <= date((SELECT films.release_date FROM films WHERE id = :id), '+15 years')`,  => {
-        
-  //       if(rows === undefined) {
-  //         return res.json({message: 'No film with id: ' + id + '.'});
-  //       }
-
-  //       // console.log(rows);
-  //       // fetchReviewsFromAPI(rows);
-  //       filmList = rows;
-
-  //       // Get ids of the filmList and transform the array into a string
-  //       const filmIDList = filmList.map(film => {
-  //         return film.id;
-  //       }).join(',');
-      
-  //       // Build the URI for the API request
-  //       let apiRequestInfo = {
-  //         uri: BASE_API_URL + '?films=' + filmIDList,
-  //         json: true
-  //       };
-
-  //       request.get(apiRequestInfo, (err, res, body)=> {
-  //         let reviewList = body[0];
-    
-  //         console.log(reviewList);
-  //         // this loop iterates throught the filmList and search
-  //         // for films which has a minimum of 5 reviews and a 
-  //         // average ratings greater than 4.0.
-  //         for(let k = 0; k < filmList.length; k++) {
-  //           if(filmList[k].id === reviewList.film_id) {
-  //               filmList[k].reviews = reviewList.reviews;
-  //           }
-  //         }
-          
-  //         for(let k = 0; k < filmList.length; k++) {
-  //           let total = 0.0;
-  //           let numOfReviews = reviewList.reviews.length;
-
-  //           filmList[k].reviews.map(review => {
-  //             total += review.rating;
-  //           })
-
-  //           filmList[k].averageRating = total/numOfReviews;
-  //         }
-
-  //         console.log(filmList[0].reviews[0]);
-          
-  //         let filterfilmsByReviews = filmList.filter((result) => {
-  //           return filmList.reviews.length >= 5;
-  //         });
-      
-  //         let filterfilmsByAvgRatings = filterfilmsByReviews.filter((result) => {
-  //           return result.averageRating > 4
-  //         });
-          
-    
-  //         let recommendedFilms = []
-  //         filterfilmsByAvgRatings.forEach( (result) => {
-  //           recommendedFilms.push({
-  //             id: result.id,
-  //             title: result.title,
-  //             releaseDate: result.releaseDate,
-  //             genre: result.genre,
-  //             averageRating: getAvgRating,
-  //             reviews: result.reviews.length
-  //           });
-  //         });
-    
-  //         // Handles offset, limit and response in json format
-  //         res.json({
-  //           recomendations: recommendedFilms.slice(offset, offset+limit),
-  //           meta: {limit: limit, offset: offset}
-  //         });
-  //       });
-         
-  //  });
-
   // Helper method get the average rating for each review. 
   function getAvgRatings(reviews) {
     let sumOfReviews = 0.0;
@@ -247,76 +157,6 @@ function getFilmRecommendations(req, res) {
     });
     return (sumOfReviews / numOfReviews).toFixed(2);
   }
-
-
-//   // This helper function will retrieve the ratings and Review from the third party API
-// function fetchReviewsFromAPI(filmList) {
-  
-//     // Get ids of the filmList and transform the array into a string
-//     const filmIDList = filmList.map(film => {
-//       return film.id;
-//     }).join(',');
-  
-//     // Build the URI for the API request
-//     let apiRequestInfo = {
-//       uri: BASE_API_URL + '?films=' + filmIDList,
-//     };
-    
-//     request.get(apiRequestInfo, (err, res, body)=> {
-//       let reviewList = body[0];
-
-//       console.log(reviewList);
-//       // this loop iterates throught the filmList and search
-//       // for films which has a minimum of 5 reviews and a 
-//       // average ratings greater than 4.0.
-//       for(let k = 0; k < filmList.length; k++) {
-//         if(filmList[k].id === reviewList.film_id) {
-//             filmList[k].reviews = reviewList.reviews;
-//             filmList[k].averageRating = getAvgRatings(reviewList.reviews);
-//         }
-//       }
-
-//       console.log(filmList[0]);
-      
-//       let filterfilmsByReviews = filmList.filter((result) => {
-//         return filmList.length >= 5;
-//       });
-  
-//       let filterfilmsByAvgRatings = filterfilmsByReviews.filter((result) => {
-//         return getAvgRatings(result) > 4
-//       });
-      
-
-//       let recommendedFilms = []
-//       filterfilmsByAvgRatings.forEach( (result) => {
-//         recommendedFilms.push({
-//           id: result.id,
-//           title: result.title,
-//           releaseDate: result.releaseDate,
-//           genre: result.genre,
-//           averageRating: getAvgRating,
-//           reviews: result.reviews.length
-//         });
-//       });
-
-//       // Handles offset, limit and response in json format
-//       res.json({
-//         recomendations: recommendedFilms.slice(offset, offset+limit),
-//         meta: {limit: limit, offset: offset}
-//       });
-//     });
-//   }
-  
-//   // Helper method get the average rating for each review. 
-//   function getAvgRatings(reviewList) {
-//     let sumOfReviews = 0.0;
-//     let numOfReviews = reviewList.length;
-    
-//     reviewList.reviews.forEach(review => {
-//       sumOfReviews += review.rating;
-//     });
-//     return sumOfReviews / numOfReviews;
-//   }
 }
 
 app.use(function(req, res, next) {
